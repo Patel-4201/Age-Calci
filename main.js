1e8a105a6d28dae113396f5ff3956486cd3773fc
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-09-24 06:40:33 +0000

    create functions

--- main.js
+++ main.js
@@ -17,19 +17,4 @@ function calculateAge() {
 }
 
 // getAge func
-function getAge(birthdayValue) {
-  const currentDate = new Date();
-  const birthdayDate = new Date(birthdayValue);
-  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-  const month = currentDate.getMonth() - birthdayDate.getMonth();
-  if (
-    month < 0 ||
-    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-  ) {
-    age--;
-  }
 
-  return age;
-}
-
-btnEl.addEventListener("click", calculateAge);
